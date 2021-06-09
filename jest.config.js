8f9aa0c2060b53907b567691394d96b470fa08db
Paulo Frare <pfrare.tads@gmail.com>
2021-06-09 00:03:47 -0300

    config tests

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  // collectCoverage: true,
+  // collectCoverageFrom: ['src/**/*.(ts|vue)'],
+  testMatch: [
+    '**/*.spec.ts'
+  ],
   transform: {
     '^.+\\.vue$': 'vue-jest'
-  }
+  },
+  moduleFileExtensions: [
+    "js",
+    "ts",
+    "json",
+    "vue"
+  ],
 }
